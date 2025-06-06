48ee77b24225c1669af4a679414d075cff497ae1
hannn2003 <hannn@iceteasoftware.com>
2025-06-06 09:19:51 +0000

    add: unit test, config account info

--- programs/project01/src/lib.rs
+++ programs/project01/src/lib.rs
@@ -1,4 +1,6 @@
 use anchor_lang::prelude::*;
+pub mod constants;
+use constants::CONFIG_SEED;
 
 declare_id!("9NRU7WAPGifEpgPT3w1rF593G6VnnGLDKMEsc4Zy5idJ");
 
@@ -8,9 +10,37 @@ pub mod project01 {
 
     pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
         msg!("Greetings from: {:?}", ctx.program_id);
+        let config = &mut ctx.accounts.config;
+        config.owner = ctx.accounts.owner.key();
+        config.creator = ctx.accounts.creator.key();
         Ok(())
     }
 }
 
 #[derive(Accounts)]
-pub struct Initialize {}
+pub struct Initialize<'info> {
+    #[account(
+        init,
+        payer = creator,
+        space = 8 + 32 + 32,
+        seeds = [CONFIG_SEED],
+        bump
+    )]
+    pub config: Account<'info, Config>,
+
+    // The owner of the platform
+    pub owner: Signer<'info>,
+
+    // The creator who pays the rent/init
+    #[account(mut)]
+    pub creator: Signer<'info>,
+
+    // System program
+    pub system_program: Program<'info, System>,
+}
+
+#[account]
+pub struct Config {
+    pub owner: Pubkey,
+    pub creator: Pubkey,
+}
--- tests/project01.ts
+++ tests/project01.ts
@@ -1,6 +1,7 @@
 import * as anchor from "@coral-xyz/anchor";
 import { Program } from "@coral-xyz/anchor";
 import { Project01 } from "../target/types/project01";
+import { assert } from "chai";
 
 describe("project01", () => {
   // Configure the client to use the local cluster.
@@ -8,9 +9,49 @@ describe("project01", () => {
 
   const program = anchor.workspace.project01 as Program<Project01>;
 
-  it("Is initialized!", async () => {
-    // Add your test here.
-    const tx = await program.methods.initialize().rpc();
-    console.log("Your transaction signature", tx);
+  // create key pair for owner
+  const owner = "FMeR43MSM64gjztwqWQSHc8hCYj82GrVTfwFzfcv9UHU";
+
+  const authority = anchor.web3.Keypair.fromSecretKey(
+    new Uint8Array(require("/home/dmin/.config/solana/id.json"))
+  );
+
+  const user = anchor.web3.Keypair.generate();
+  const operator = anchor.web3.Keypair.generate();
+
+  before(async () => {
+    const provider = anchor.AnchorProvider.env();
+    const latestBlockHash = await provider.connection.getLatestBlockhash();
+
+    await Promise.all(
+      [user, operator].map(async (account) => {
+        const airdropSignature = await provider.connection.requestAirdrop(
+          account.publicKey,
+          1_000 * 1e9
+        );
+
+        await provider.connection.confirmTransaction({
+          blockhash: latestBlockHash.blockhash,
+          lastValidBlockHeight: latestBlockHash.lastValidBlockHeight,
+          signature: airdropSignature,
+        });
+      })
+    );
+  });
+
+  const configAccount = anchor.web3.PublicKey.findProgramAddressSync(
+    [Buffer.from("ido_platform_seed")],
+    program.programId
+  )[0];
+
+  describe("Configuration", () => {
+    it("Should initialize successfully", async () => {
+      await program.methods.initialize().rpc();
+
+      const configInfo = await program.account.config.fetch(configAccount);
+      console.log("configInfo", configInfo);
+      assert.equal(configInfo.owner.toBase58(), owner);
+      assert.equal(configInfo.creator.toBase58(), owner);
+    });
   });
 });
